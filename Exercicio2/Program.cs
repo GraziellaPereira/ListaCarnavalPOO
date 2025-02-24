352f608fb2d62104d56d21b0067c73eb902e051f
graziella.pereira <graziellapereira52@gmail.com>
2025-02-24 20:04:44 -0300

    adicionado exercicio 2 (corrigir)

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, pesoIdeal;
+            string sexo;
+            Console.WriteLine("Vamos descobrir o seu peso ideal.");
+            Console.WriteLine("Digite seu sexo (F ou M): ");
+            sexo = Console.ReadLine().ToUpper();
+            if (sexo != "F" && sexo != "M")
+            {
+                Console.WriteLine("Digite M ou F!");
+                return;
+
+            }
+            else
+            {
+                
+                if (sexo == "F")
+                {
+                    pesoIdeal = (62.1 * h) - 44.7;
+                }
+                else
+                {
+                    pesoIdeal = (72.7 * h) - 58;
+                }
+
+            }
+           
+            Console.ReadKey();
+        } 
+    }
+}
