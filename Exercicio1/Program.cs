dfb95b3cecb5c243835746bdf56762460cdb251b
graziella.pereira <graziellapereira52@gmail.com>
2025-02-24 19:37:29 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vendaMedia, precoAtual, precoReajustado;
+            Console.WriteLine("Vamos descobrir o valor do reajuste do seu produto.");
+            Console.WriteLine("Digite o número da venda média mensal: ");
+            vendaMedia = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o preço atual: ");
+            precoAtual = double.Parse(Console.ReadLine());
+
+            if (vendaMedia < 500 && precoAtual < 30.00)
+            {
+                precoReajustado = ((precoAtual * 0.1) + precoAtual);
+
+                Console.WriteLine("Preço reajustado: " + precoReajustado);
+            } else if (vendaMedia >= 500 &&  precoAtual >= 30.00)
+            {
+                precoReajustado = (precoAtual - (precoAtual * 0.2));
+                Console.WriteLine("Preço reajustado: " + precoReajustado);
+            } else
+            {
+                Console.WriteLine("Não houve reajuste no preço.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
