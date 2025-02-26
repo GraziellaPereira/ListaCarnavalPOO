a90cd6162a6a9430e3b6eaf2680ba21206873617
graziella.pereira <graziellapereira52@gmail.com>
2025-02-26 18:59:49 -0300

    Exercicio 3 finalizado. Lembrar de continuar o exercicio4.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan horasMes;
+            double salarioHora, salarioTotal;
+
+            Console.WriteLine("Vamos calcular seu salário considerando horas extras. ");
+            Console.WriteLine("Digite quantas horas foram trabalhadas no mês: ")
+            horasMes = TimeSpan.Parse(Console.ReadLine());
+            Console.WriteLine("Ótimo. Agora digite o seu salário por hora: ");
+            salarioHora = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
